964f87208e3834b249def4617073e845a6eed49a
Alexander Fejari <alexander.fejari@gmail.com>
2023-10-04 19:41:25 +0200

    header wip on collaspe menu mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
 import '../css/main.css';
+
+// toggle menu mobile
+const mobileMenu = document.querySelector('nav ul');
+const toggleButton = document.querySelector('button');
+
+toggleButton.addEventListener('click', () => {
+    if (mobileMenu.classList.contains('block')) {
+        mobileMenu.classList.remove('block');
+        mobileMenu.classList.add('hidden');
+    } else {
+        mobileMenu.classList.remove('hidden');
+        mobileMenu.classList.add('block');
+    }
+});

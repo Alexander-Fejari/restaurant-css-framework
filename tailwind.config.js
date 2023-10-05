afdb5159bf5304da14c4d74f6aa21f76a72f374f
Alexander Fejari <alexander.fejari@gmail.com>
2023-10-05 10:30:35 +0200

    header bug menu mobile nav ne se cente pas

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ export default {
             fontFamily: {
                 sans: ['Roboto', 'sans-serif'],
             },
+            keyframes: {
+                fadeInTop: {
+                    '0%': { opacity: '0', transform: 'translateY(-50px)' },
+                    '100%': { opacity: '1', transform: 'translateY(0)' },
+                },
+            },
+            animation: {
+                fade: 'fadeInTop 1s linear',
+            },
         },
     },
     plugins: [],

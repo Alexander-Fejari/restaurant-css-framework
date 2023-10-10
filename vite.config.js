ffa78cf2967614aa0f4e977a01e020c84587f780
Alexander Fejari <alexander.fejari@gmail.com>
2023-10-10 22:44:14 +0200

    vite config modif

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import {resolve} from "path";
 
-const root = resolve(dirname, 'src');
+const root = resolve(dirname, './');
 const outDir = resolve(dirname, 'dist');
 
 export default {
@@ -13,7 +13,7 @@ export default {
             input: {
                 main: resolve(root, 'index.html'),
                 menu: resolve(root, 'menu', 'menu.html'),
-                about_us: resolve(root, 'about_us', 'restaurant.html'),
+                about_us: resolve(root, 'about_us', 'restaurants.html'),
                 gallery: resolve(root, 'gallery', 'gallery.html'),
                 contact: resolve(root, 'contact', 'contacte.html'),
             }

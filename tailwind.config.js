6c31ba28c9c6669d7a2b1a6956047cd2d19ec63e
code4cake <dantevs@icloud.com>
2023-10-15 08:52:42 +0200

    fix/bug-pictures-deployment: Install dependencies. Update .gitignore files to ignore files fro yarn@v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./*.html', './src/**/*.js'],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Roboto', 'sans-serif'],
-            },
-            screens: {
-                'smd': '770px',
-            },
-            keyframes: {
-                fadeInTop: {
-                    '0%': {opacity: '0', transform: 'translateY(-50px)'},
-                    '100%': {opacity: '1', transform: 'translateY(0)'},
-                },
-            },
-            animation: {
-                fade: 'fadeInTop 1s linear',
-            },
+  content: ["./*.html", "./src/**/*.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
+      screens: {
+        smd: "770px",
+      },
+      keyframes: {
+        fadeInTop: {
+          "0%": { opacity: "0", transform: "translateY(-50px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
         },
+      },
+      animation: {
+        fade: "fadeInTop 1s linear",
+      },
     },
-    plugins: [require('daisyui')],
+  },
+  plugins: [require("daisyui")],
 };
